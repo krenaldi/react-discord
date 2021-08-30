3ffb4a0769f2b355bcc4ad1a057496372c56ee1a
Kris Renaldi <krenaldi@comcast.net>
2021-08-30 00:17:17 -0400

    Added http interceptor to redirect user to login page if if something is wrong with user data

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -5,3 +5,20 @@ export const api = axios.create({
   // always send credentials for every http request in form of cookie containing jwt
   withCredentials: true
 });
+
+// HTTP interceptor to intercept a response
+api.interceptors.response.use(response => {
+  return response;
+}, error => {
+  const { status } = error.response;
+  switch (status) {
+    case 403:
+      localStorage.removeItem("user-storage");
+      window.location.replace("/login")
+      break;
+
+    default:
+      break;
+  }
+  return Promise.reject(error);
+})
