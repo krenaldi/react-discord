8862fd997546628fa6063700fabf4477f08ddefa
Kris Renaldi <krenaldi@comcast.net>
2021-09-02 19:01:43 -0400

    Display user DMs in DM Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import Account from "./pages/Account";
 import AuthRoute from "./pages/AuthRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
-// import Invite from "./pages/Invite";
+import Invite from "./pages/Invite";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ResetPassword from "./pages/ResetPassword";
-// import ViewGuild from "./pages/ViewGuild";
+import ViewGuild from "./pages/ViewGuild";
 
 export default function App() {
   return (
